d3cdfd91a3fcb7bc41923d12dace896c0049bf31
Reagan Barrington <reagan@barringtonfamily.net>
2020-09-01 18:14:12 -0400

    running on port 5000

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const server = express();
 
-const charactersRouter = require('./schema/characters-router.js');
+const booksRouter = require('./src/schema/books-router');
 
 server.get('/', (req, res) => {
-    res.send(`<h2>Sprint!</h2>`);
+    res.send(`<h2>Working!</h2>`);
   });
 
 server.use(express.json()); // built-in middleware, no need to install it
 
 //endpoints
-server.use('/api/characters', charactersRouter);
+server.use('/api/books', booksRouter);
 
 
 module.exports = server;
